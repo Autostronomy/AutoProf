45f70c7b2eed09b91006bdc1f284f36e98717bd1
Connor Stone <connorstone628@gmail.com>
2021-04-22 15:16:34 -0400

    bug in test for forced photometry due to new filename changes

--- test/test_forced_config.py
+++ test/test_forced_config.py
@@ -2,7 +2,7 @@
 ap_process_mode = 'forced image'
 
 ap_image_file = 'test_ESO479-G1_r.fits'
-ap_forcing_profile = 'testimage.prof'
+ap_forcing_profile = 'testimage_AP.prof'
 ap_pixscale = 0.262
 ap_name = 'testforcedimage'
 ap_doplot = True
