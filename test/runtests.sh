0621285d2c5baee6fb96f5777a77f96ca5e09fcd
Connor Stone <connorstone628@gmail.com>
2021-04-23 10:55:11 -0400

    tests now automatically check for errors

--- test/runtests.sh
+++ test/runtests.sh
@@ -6,4 +6,6 @@ echo "batch test"
 autoprof test_batch_config.py Batch.log &> output_batch.txt
 echo "decision tree test"
 autoprof test_tree_config.py Tree.log &> output_tree.txt
+echo "checking for errors (will be written below if any):"
+grep ERROR *.log
 echo "all done!"
