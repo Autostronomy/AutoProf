5656714e21431f4fa039a583f5073790c518e6ca
Connor Stone <connorstone628@gmail.com>
2021-05-06 09:45:22 -0400

    organizing test configfile commands

--- test/test_config.py
+++ test/test_config.py
@@ -2,8 +2,8 @@
 ap_process_mode = 'image'
 
 ap_image_file = 'test_ESO479-G1_r.fits'
-ap_pixscale = 0.262
 ap_name = 'testimage'
+ap_pixscale = 0.262
 ap_doplot = True
 ap_isoband_width = 0.05
 ap_samplegeometricscale = 0.05
