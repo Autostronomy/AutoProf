ff50d695f4b55e7d38cb7a8b2e0e75d319bc3504
Connor Stone <connorstone628@gmail.com>
2021-06-28 16:06:41 -0400

    and again.........

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,8 @@ release = '0.8'
 # ones.
 extensions = [
     'sphinx_rtd_theme',
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
