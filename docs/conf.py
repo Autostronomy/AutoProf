306ff6103bcf561f88641d0f5deef5b0a8901343
Connor Stone <connorstone628@gmail.com>
2024-03-05 22:35:22 -0500

    fix rtd config

--- docs/conf.py
+++ docs/conf.py
@@ -13,22 +13,24 @@
 
 import os
 import sys
+
 sys.path.append("/home/connor/Programming/AutoProf/autoprof")
 import autoprof
 import sphinx_rtd_theme
-#import autoprofutils
+
+# import autoprofutils
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'AutoProf'
-copyright = '2021, Connor Stone'
-author = 'Connor Stone'
+project = "AutoProf"
+copyright = "2021, Connor Stone"
+author = "Connor Stone"
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = "1.1"
 
-master_doc = 'index'
+master_doc = "index"
 
 # -- General configuration ---------------------------------------------------
 
@@ -36,21 +38,21 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
-    'sphinx.ext.autodoc',
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'autoprof.py']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "autoprof.py"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -58,12 +60,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'autoprof.py']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
-#autodoc_mock_imports = ['numpy', 'scipy', 'matplotlib', 'astropy', 'photutils', 'sklearn']
+# autodoc_mock_imports = ['numpy', 'scipy', 'matplotlib', 'astropy', 'photutils', 'sklearn']
